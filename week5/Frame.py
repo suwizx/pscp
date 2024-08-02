ff548c3dd6cb3abcda4b04901ea724905eafd25c
Suwijak Promsatid <suwijak.pak@gmail.com>
2024-08-02 09:13:35 +0700

    week5

---
+++ week5/Frame.py
@@ -0,0 +1,11 @@
+"""sijijfeijes"""
+
+def main():
+    """eakioeis"""
+    text = input()
+    twxt_len = len(text)
+    print("*" + ("*" * twxt_len) + "*")
+    print("*" + text + "*")
+    print("*" + ("*" * twxt_len) + "*")
+
+main()
